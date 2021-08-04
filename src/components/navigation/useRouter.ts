3840d8741a7ca7d38af3f9551da47de151603086
Konstantin Barabanov <criitch@yandex.ru>
2021-08-04 16:10:34 +0300

    feat: typed router.params

--- src/components/navigation/useRouter.ts
+++ src/components/navigation/useRouter.ts
@@ -1,15 +1,34 @@
 import React from 'react';
+import { ExtractRouteParams, RouteProps } from 'react-router';
 import { useLocation, useRouteMatch, useHistory, useParams } from 'react-router-dom';
 
 function useQuery(search: string) {
   return React.useMemo(() => new URLSearchParams(search), [search]);
 }
 
-function useRouter() {
+/**
+ * Stringify object type values
+ * @example { val: string | number | boolean } -> { val: string }
+ */
+type StringifyTypeValues<T extends Partial<Record<string, string | number | boolean>>> = Record<keyof T, string>;
+
+interface UseRouterProps<Route extends string> {
+  matchRoute?: Route | Route[] | RouteProps;
+}
+
+/**
+ * Router info
+ * @param matchRoute - expected route to match
+ */
+function useRouter<Route extends string>({ matchRoute }: UseRouterProps<Route> = {}) {
   const location = useLocation();
-  const routeMatch = useRouteMatch();
+  const routeMatch = matchRoute
+    ? // eslint-disable-next-line react-hooks/rules-of-hooks -- call same hook, skip eslint error
+      useRouteMatch<StringifyTypeValues<ExtractRouteParams<Route>>>(matchRoute)
+    : // eslint-disable-next-line react-hooks/rules-of-hooks -- call same hook, skip eslint error
+      useRouteMatch<StringifyTypeValues<ExtractRouteParams<Route>>>();
   const history = useHistory();
-  const params = useParams<Partial<Record<string, string>>>();
+  const params = useParams<StringifyTypeValues<ExtractRouteParams<Route>>>();
   const query = useQuery(location.search);
 
   const router = React.useMemo(
